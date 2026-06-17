275696f04e4108c733714a3c7f117533fe6f5e3e
Monirul Islam <monirulislammredul@gmail.com>
2026-06-17 15:42:09 +0600

    chore: remove compiled views, cache, sessions, testing from git tracking

--- storage/framework/views/33f59b9801df3ce0d475dab6d55a1d371322c4b7.php
+++
@@ -1,6 +0,0 @@
-<?php $footer = \App\Models\FooterDetail::latest()->first() ?>
-<footer class="footer">
-    © <?php echo e(date('Y-m-d')); ?> <?php echo e($footer->credit?? 'Designed by Techweb BD IT'); ?>
-
-</footer>
-<?php /**PATH C:\laragon\www\Madobi_jewelers\resources\views/admin/include/footer.blade.php ENDPATH**/ ?>
